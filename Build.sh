eaceeca9b8b347b20f639c0dacce57d69598750b
OpenCo <admin@openco.ca>
2023-12-31 08:55:32 -0100

    deploy

--- Build.sh
+++ Build.sh
@@ -27,6 +27,9 @@ echo      BRANCH=$BRANCH
 
 # Build the Docker image using the docker build command, and tag it with the openco/$PROJECT-$BRANCH:$TAG tag
 # Then tag the image with the openco/$PROJECT-$BRANCH:latest tag, and run the ./Run.sh script
+
+#Dockerfile.prerequisites.linux_arm \
+
 echo docker build -t openco/$PROJECT-$BRANCH:$TAG .
 echo docker tag -f openco/$PROJECT-$BRANCH:$TAG openco/$PROJECT-$BRANCH:latest
 docker build \
